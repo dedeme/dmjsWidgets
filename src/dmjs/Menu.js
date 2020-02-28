3675e02b00e361ad7da03a36c95811708fd0bf4e
Deme <deme@debian.es>
2020-02-28 19:48:56 +0100

    Modified Menu

--- src/dmjs/Menu.js
+++ src/dmjs/Menu.js
@@ -1,175 +1,207 @@
-// Copyright 18-Jul-2019 ºDeme
+// Copyright 28-Jan-2020 ºDeme
 // GNU General Public License - V3 <http://www.gnu.org/licenses/>
 
-import Domo from "../dmjs/Domo.js"; //eslint-disable-line
-import Ui from "../dmjs/Ui.js";
+/**
+    Menu widget.
+**/
+
+import Domo from "./Domo.js"; //eslint-disable-line
+import Ui from "./Ui.js";
+import Maybe from "./Maybe.js";
 
 const $ = e => Ui.$(e);
 
 /**
-    Menu widget.
-***/
-export default class Menu {
+    Menu entry.
+**/
+export class MenuEntry {
+  /**
+      @param { Maybe<string>} id
+      @param {!Domo} wg
+  **/
+  constructor (id, wg) {
+
+    /**
+        @private
+        @type { Maybe<string>}
+    **/
+    this._id = id;
+
+    /**
+        @private
+        @type {!Domo}
+    **/
+    this._wg = wg;
+
+  }
 
   /**
+      @return { Maybe<string>}
+  **/
+  get id () {
+    return this._id;
+  }
+
+  /**
+      @return {!Domo}
+  **/
+  get wg () {
+    return this._wg;
+  }
+}
+
+/**
+    Menu widget.
+**/
+export class Menu {
+
+  /**
+      @param {!Array<!MenuEntry>} lopts
+      @param {!Array<!MenuEntry>} ropts
+      @param {string} selected
       @param {boolean=} withSeparator (Default false)
   **/
-  constructor (withSeparator = false) {
+  constructor (lopts, ropts, selected, withSeparator = false) {
+    function setId (o) {
+      if (o.id.isJust())
+        o.wg.klass(o.id.fromJust() === selected ? "frame" : "link");
+    }
+    lopts.forEach(o => setId(o));
+    ropts.forEach(o => setId(o));
+
+    /**
+        @private
+    **/
+    this._lopts = lopts;
+    /**
+        @private
+    **/
+    this._ropts = ropts;
     /**
         @private
     **/
     this._withSeparator = withSeparator;
-    /**
-        @private
-        @type {string}
-    **/
-    this._selected = "";
-    /**
-        @private
-        @type {!Array<!Domo>}
-    **/
-    this._lopts = [];
-    /**
-        @private
-        @type {!Array<!Domo>}
-    **/
-    this._ropts = [];
-    /**
-        @private
-        @type {!Domo}
-    **/
-    this._lview = $("div").style("line-height:22px;");
-    /**
-        @private
-        @type {!Domo}
-    **/
-    this._rview = $("div");
+
+    this._wg = $("div");
+    this.view();
+  }
+
+  /**
+      @return {!Domo}
+  **/
+  get wg () {
+    return this._wg;
   }
 
   // View ----------------------------------------------------------------------
 
   /**
-    @return {!Domo}
+    @return {void}
   **/
-  get wg () {
-    return $("div")
+  view () {
+    this._wg
       .add($("table").klass("main").add($("tr")
-        .add($("td").style(
-          "padding-right:4px;" +
-          (this._withSeparator ? "border-right: 1px solid #000000;" : "")
-        ).add(this._lview))
-        .add($("td").style(
-          "padding-left:4px;vertical-align:top;" +
-          "text-align:right;white-space:nowrap"
-        ).add(this._rview))))
+        .add($("td")
+          .style(`
+            padding-right:4px;
+            line-height:22px;
+            ${this._withSeparator ? "border-right: 1px solid #000000;" : ""}`
+          )
+          .adds(this._lopts.map(e => e.wg)))
+        .add($("td")
+          .style(`
+            padding-left:4px;vertical-align:top;
+            text-align:right;white-space:nowrap`
+          )
+          .adds(this._ropts.map(e => e.wg)))))
       .add($("hr"))
     ;
-  }
-
-  // Control -------------------------------------------------------------------
-
-  /**
-      Removes all items from menu.
-      @return {void}
-  **/
-  reset () {
-    this._lopts = [];
-    this._lview.removeAll();
-    this._ropts = [];
-    this._rview.removeAll();
-  }
-
-  /**
-      Adds elements to left part of menu. Elements are added left to right.
-      @param {!Domo} o
-      @return {void}
-  **/
-  addLeft (o) {
-    this._lopts.push(o);
-    this._lview.removeAll().adds(this._lopts);
-  }
-
-  /**
-      Adds elements to right part of menu. Elements are added right to left.
-      @param {!Domo} o
-      @return {void}
-  **/
-  addRight (o) {
-    this._ropts.unshift(o);
-    this._rview.removeAll().adds(this._ropts);
-  }
-
-  /**
-      Marks an option as selected.
-      @param {string} id
-      @return {void}
-  **/
-  setSelected (id) {
-    const mId = "menu_" + id;
-    function setId (o) {
-      const att = o.att("id");
-      if (att && att !== "")
-        o.klass(att === mId ? "frame" : "link");
-    }
-    this._lopts.forEach(o => setId(o));
-    this._ropts.forEach(o => setId(o));
   }
 
   // Static --------------------------------------------------------------------
 
   /**
-      @return {!Domo}
+      @return {!MenuEntry}
   **/
   static separator () {
-    return $("span").text(" · ");
+    return new MenuEntry(Maybe.nothing, $("span").text(" · "));
   }
 
   /**
-      @return {!Domo}
+      @return {!MenuEntry}
   **/
   static separator2 () {
-    return $("span").text(" | ");
+    return new MenuEntry(Maybe.nothing, $("span").text(" | "));
   }
 
   /**
       @param {string} id
       @param {string} tx
       @param {function():(void|!Promise)} f
-      @return {!Domo}
+      @return {!MenuEntry}
   **/
-  static mkOption (id, tx, f) {
-    return Ui.link(f)
-      .att("id", "menu_" + id).text(tx);
+  static toption (id, tx, f) {
+    return new MenuEntry(Maybe.just(id), Ui.link(f).text(tx));
   }
 
   /**
-      Link is formed 'Main.urlBase + "?" + module + "&" + id' or
-      'Main.urlBase + "?" + id' if module is not defined.
+      @param {string} id
+      @param {string} img Image name. If it has not extension, '.png' will be
+                          used. It must be placed in a directory named 'img'.
+      @param {function():(void|!Promise)} f
+      @return {!MenuEntry}
+  **/
+  static ioption (id, img, f) {
+    return new MenuEntry(
+      Maybe.just(id),
+      Ui.link(f).add(Ui.img(img).style("vertical-align:middle"))
+    );
+  }
+
+  /**
+      Link is formed '"?" + module + "&" + id' or '"?" + id' if module is not
+      defined.
       @param {string} id
       @param {string} tx
       @param {string=} module
-      @return {!Domo}
+      @return {!MenuEntry}
   **/
-  static mkLink (id, tx, module) {
-    let path = window.location.href;
-    let ix = path.indexOf("?");
-    path = ix === -1 ? path : path.substring(0, ix);
-    ix = path.indexOf("#");
-    path = ix === -1 ? path : path.substring(0, ix);
+  static tlink (id, tx, module) {
+    return new MenuEntry(
+      Maybe.just(id),
+      $("a")
+        .att("href", "?" + (module === undefined ? "" : module + "&") + id)
+        .text(tx)
+    );
+  }
 
-    return $("a")
-      .att("href", path + "?" + (module === undefined ? "" : module + "&") + id)
-      .att("id", "menu_" + id)
-      .text(tx)
-    ;
+  /**
+      Link is formed '"?" + module + "&" + id' or '"?" + id' if module is not
+      defined.
+      @param {string} id
+      @param {string} img Image name. If it has not extension, '.png' will be
+                          used. It must be placed in a directory named 'img'.
+      @param {string=} module
+      @return {!MenuEntry}
+  **/
+  static ilink (id, img, module) {
+    return new MenuEntry(
+      Maybe.just(id),
+      $("a")
+        .att("href", "?" + (module === undefined ? "" : module + "&") + id)
+        .add(Ui.img(img))
+    );
   }
 
   /**
       @param {function ():?} fbye
-      @return {!Domo}
+      @return {!MenuEntry}
   **/
-  static mkClose (fbye) {
-    return Ui.link(fbye).add(Ui.img("cross").style("vertical-align:bottom"));
+  static close (fbye) {
+    return new MenuEntry(
+      Maybe.nothing,
+      Ui.link(fbye).add(Ui.img("cross").style("vertical-align:middle"))
+    );
   }
 
 }
